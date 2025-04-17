b2aaa60dd83e2a69c1ac0e1964a8238771c55af3
RileyTernes <riley.ternes@gmail.com>
2025-04-17 13:53:39 -0500

    Update ACME.py

--- ACME.py
+++ ACME.py
@@ -1,1 +1,21 @@
 #ACME
+import RetailItem
+import CashRegister
+
+def main():
+    #calls menu to get choice
+    choice = menu()
+    #makes a decision based off of choice
+    #calls the functions or quits
+    if choice == 1:
+        #do retail
+        pass
+    elif choice == 2:
+        #do cash register
+    elif choice == 3:
+        quit()
+    
+def menu():
+    #prints the menu
+    #gets choice
+    #validates choice and returns choice
