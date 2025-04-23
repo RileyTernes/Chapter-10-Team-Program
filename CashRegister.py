ed7d5905a043884b8b5aa2bc32f854a11f9bd133
OwenSchroeder <owenschr14@gmail.com>
2025-04-23 13:51:28 -0500

    Update CashRegister.py

--- CashRegister.py
+++ CashRegister.py
@@ -1,4 +1,16 @@
 #CashRegister
-
+dictionary = []
+count = 0
 class CashRegister:
+    
+    
+    def purchase_item(self, RetailItem)
+        dictionary += RetailItem
+        
+    def get_total(self)
+        for stuff in dictionary:
+            count += dictionary[stuff]
+            
+    def get_cart
+            
 
